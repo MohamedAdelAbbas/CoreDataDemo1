0da6c9c35d47ccc7621ce0de7358d2f93b5811c9
MohamedAdelAbbas <m.3del95@gmail.com>
2020-05-25 00:42:44 +0200

    Initial Commit

---
+++ CoreDataDemo1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDemo1
+//
+//  Created by Mohamed Adel on 5/25/20.
+//  Copyright © 2020 Mohamed Adel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
