811682469fe9ce44dd9fd7383a466e637236d6fe
MohamedAdelAbbas <m.3del95@gmail.com>
2020-05-25 02:18:09 +0200

    Build Structure of CoreData

---
+++ CoreDataDemo1/Controller/ProductVC.swift
@@ -0,0 +1,35 @@
+//
+//  ProductVC.swift
+//  CoreDataDemo1
+//
+//  Created by Mohamed Adel on 5/25/20.
+//  Copyright © 2020 Mohamed Adel. All rights reserved.
+//
+
+import UIKit
+
+class ProductVC: UIViewController {
+    // MARK: Outlets
+    
+    @IBOutlet weak var PNameTF: UITextField!
+    @IBOutlet weak var pPriceTF: UITextField!
+    @IBOutlet weak var switchFav: UISwitch!
+    // MARK: Properties
+    
+    // MARK: View Controller Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    // MARK: Action
+    
+    @IBAction func upload(_ sender: UIButton) {
+        
+    }
+    // MARK: Class Methods
+    
+    // MARK: Self Defined Methods
+
+   
+}
